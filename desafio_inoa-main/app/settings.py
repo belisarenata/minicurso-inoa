860662bcd0dfd57e9d6575cd13d0f2caf53fce06
Belisa R Arnhold <40744589+belisarenata@users.noreply.github.com>
2022-09-10 21:18:05 -0300

    Add files via upload

---
+++ desafio_inoa-main/app/settings.py
@@ -0,0 +1,10 @@
+from datetime import timedelta
+#from celery.schedules import crontab
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = "2+b7@f++-$lb4y*hs+y)yfc(3uq%ttv+9yi=v91s^r8dqk$7i7"
+
+DEBUG = True
